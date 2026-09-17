ad372b126ff3ed858d880541804b91427a637305
Yan Vashchanka <yan.vashchenko@gmail.com>
2026-09-17 20:31:35 +0300

    feat: task1

--- task1.ts
+++ task1.ts
@@ -1,4 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 // Написать функцию, которая возвращает новый массив только положительных чисел, умноженный вдвое
 
 const array: number[] = [1, -5, 7, 8, -9, 0, -4];
+
+function getPositiveDoubles(arr: number[]) {
+  return arr.filter((x) => x >= 0).map((x) => x * x);
+}
+
+console.log(getPositiveDoubles(array));
